9fb4e1227c8f6fb905344cfe5239aa4abec116f8
Joachim Takoutsing <joachim.takoutsing@uni-weimar.de>
2013-06-11 08:59:36 +0200

    Reichstag app

---
+++ bt.js
@@ -0,0 +1,43 @@
+var myImage= new Array(); 
+myImage[0]="20.jpg";       
+myImage[1]="t2.jpg";
+myImage[2]="t3.jpg";
+myImage[3]="t4.jpg"; 
+myImage[4]="t5.jpg";
+myImage[5]="t6.jpg";
+myImage[6]="t7.jpg";
+myImage[7]="t8.jpg";
+var ImageCnt = 0;
+/*
+function next(){
+    ImageCnt++;
+    document.getElementById("whiteBox").style.background = 'url(' + myImage[ImageCnt] + ')';
+  }
+
+  function BHnav()
+  {
+  document.location.href ='index.html';
+  }
+  function BMnav()
+  {
+  document.location.href ='map.html';
+  }
+  function BMnav()
+  {
+  document.location.href ='tweets.html';
+  }
+  function BAnav()
+  {
+  document.location.href ='about.html';
+  }
+  function BCnav()
+  {
+  document.location.href ='contact.html';
+  }
+  function BPnav()
+  {
+  document.location.href ='pictures.html';
+  }
+  
+ 
+*/
